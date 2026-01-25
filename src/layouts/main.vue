9f9429f6650bc699ac4769ae82344d3b91132c8b
yukin <yukin371@outlook.com>
2026-01-25 10:13:48 +0800

    feat(router): create layout files (main, writer, blank)

---
+++ src/layouts/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-layout">
+    <AppHeader />
+    <main class="content">
+      <router-view />
+    </main>
+    <BackTop />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from '@/components/Layout/Header.vue'
+import BackTop from '@/design-system/other/BackTop/BackTop.vue'
+</script>
+
+<style scoped lang="scss">
+.main-layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  flex: 1;
+}
+</style>
