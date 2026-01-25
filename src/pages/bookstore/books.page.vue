83a8833d6c6421da5c0c7bcd8b99d1cc369c79b4
yukin <yukin371@outlook.com>
2026-01-25 10:15:18 +0800

    feat(router): add bookstore test pages

---
+++ src/pages/bookstore/books.page.vue
@@ -0,0 +1,17 @@
+<template>
+  <BooksView />
+</template>
+
+<script setup lang="ts">
+import BooksView from '@/modules/bookstore/views/BooksView.vue'
+</script>
+
+<route lang="json5">
+{
+  name: 'bookstore:books',
+  meta: {
+    title: '书库',
+    requiresAuth: false
+  }
+}
+</route>
---
+++ src/pages/bookstore/index.page.vue
@@ -0,0 +1,18 @@
+<template>
+  <HomeView />
+</template>
+
+<script setup lang="ts">
+import HomeView from '@/modules/bookstore/views/HomeView.vue'
+</script>
+
+<route lang="json5">
+{
+  name: 'bookstore:home',
+  meta: {
+    title: '首页',
+    requiresAuth: false,
+    layout: 'main'
+  }
+}
+</route>
