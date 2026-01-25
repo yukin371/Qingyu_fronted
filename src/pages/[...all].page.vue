354b20f7d0ec7db6080f329322b31b4183e436af
yukin <yukin371@outlook.com>
2026-01-25 10:14:51 +0800

    feat(router): add 404 catch-all page

---
+++ src/pages/[...all].page.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>页面未找到</p>
+    <RouterLink to="/bookstore">返回首页</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped lang="scss">
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 50vh;
+  text-align: center;
+}
+h1 {
+  font-size: 72px;
+  margin: 0;
+}
+</style>
